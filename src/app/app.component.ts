d0cf717f5ed0e4bd0102ebc883e12c2df30d78ec
ericsvendsen <esvendsen@gmail.com>
2017-12-11 15:35:40 -0500

    started adding login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Message } from 'primeng/primeng';
 
 @Component({
     selector: 'seed-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'Seed Vault';
+    authenticated: boolean;
+    apiUrl = 'http://ec2-18-217-60-133.us-east-2.compute.amazonaws.com';
+    loading: boolean;
+    loginIcon = 'fa-sign-in';
+    username: string;
+    password: string;
+    userData: any;
+    msgs: Message[] = [];
+
+    constructor(
+        private http: Http
+    ) {}
+
+    private doLogin(creds) {
+        this.loading = true;
+        this.loginIcon = 'fa-spinner fa-spin';
+        return this.http.post(`${this.apiUrl}/login`, creds)
+            .toPromise()
+            .then(response => {
+                this.loading = false;
+                this.loginIcon = 'fa-sign-in';
+                this.authenticated = true;
+                return response.json();
+            })
+            .catch(this.handleError);
+    }
+
+    login(): any {
+        if (!this.authenticated) {
+            this.doLogin({ username: this.username, password: this.password }).then(data => {
+                this.userData = JSON.stringify(data);
+            }).catch(err => {
+                const detail = err.statusText.length > 0 ? err.statusText : 'Server error';
+                this.msgs = [{severity: 'error', summary: 'Login Failed', detail: detail}];
+                this.loading = false;
+                this.loginIcon = 'fa-sign-in';
+            });
+        }
+    }
+
+    private handleError(error: any): Promise<any> {
+        return Promise.reject(error.message || error);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+
+import { ButtonModule, GrowlModule, InputTextModule, OverlayPanelModule } from 'primeng/primeng';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SeedImagesModule } from './seed-images/seed-images.module';
@@ -16,7 +19,13 @@ import { StateService } from './state.service';
         AppRoutingModule,
         BrowserAnimationsModule,
         BrowserModule,
+        FormsModule,
         HttpModule,
+        // primeng
+        ButtonModule,
+        GrowlModule,
+        InputTextModule,
+        OverlayPanelModule,
         SeedImagesModule
     ],
     providers: [
